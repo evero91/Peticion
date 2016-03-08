ad8c8a19dd8c1412c7d0b46e545fceb9b2d0db8d
Everardo Guevara <kaz.msf@gmail.com>
2016-03-08 09:34:34 -0600

    Actualización Tarea Semana 2: Procesando el resultado de la petición al servidor openlibrary.org

--- Peticion/ViewController.swift
+++ Peticion/ViewController.swift
@@ -10,24 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var txtContenido: UITextView!
     @IBOutlet weak var txtISBN: UITextField!
+    @IBOutlet weak var lblTitulo: UILabel!
+    @IBOutlet weak var txtAutores: UITextView!
+    @IBOutlet weak var imgPortada: UIImageView!
+    @IBOutlet weak var lblEstadoPeticion: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         txtISBN.text = "978-84-376-0494-7"
-        txtContenido.layer.borderWidth = 1
-        txtContenido.layer.borderColor = UIColor.blackColor().CGColor
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        lblTitulo.text = ""
+        txtAutores.text = ""
+        lblEstadoPeticion.text = ""
     }
     
     @IBAction func limpiar() {
-        txtContenido.text = ""
         txtISBN.text = ""
+        lblTitulo.text = ""
+        txtAutores.text = ""
+        lblEstadoPeticion.text = ""
+        imgPortada.image = nil
     }
     
     @IBAction func backgroundTap(sender: UIControl) {
@@ -35,15 +37,47 @@ class ViewController: UIViewController {
     }
     
     @IBAction func textFieldDoneEditing(textField: UITextField) {
+        lblEstadoPeticion.text = ""
         let url = NSURL(string: "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:\(txtISBN.text!)")
-        if let datos = NSData(contentsOfURL: url!) {
-            self.txtContenido.textColor = UIColor.blackColor()
-            self.txtContenido.text = String(data: datos, encoding: NSUTF8StringEncoding)
-        } else {
-            self.txtContenido.textColor = UIColor.redColor()
-            self.txtContenido.text = "Error: No hubo respuesta"
-        }
         
+        if let datos = NSData(contentsOfURL: url!) {
+            
+            do {
+                let json = try NSJSONSerialization.JSONObjectWithData(datos, options: NSJSONReadingOptions.MutableLeaves)
+                let dicJson = json as! NSDictionary
+                
+                if let dicISBN = dicJson["ISBN:\(txtISBN.text!)"] as! NSDictionary! {
+                    
+                    lblTitulo.text = dicISBN["title"] as! NSString as String
+                    imgPortada.image = nil
+                    
+                    if let dicCover = dicISBN["cover"] as! NSDictionary! {
+                        if let url  = NSURL(string: dicCover["medium"] as! NSString as String), data = NSData(contentsOfURL: url) {
+                            imgPortada.image = UIImage(data: data)
+                        }
+                    }
+                    
+                    let dicAutores = dicISBN["authors"] as! NSArray
+                    var autores = ""
+                    
+                    for autor in dicAutores {
+                        autores += "- \(autor["name"]!!)\n"
+                    }
+                    
+                    txtAutores.text = autores
+
+                } else {
+                    limpiar()
+                    lblEstadoPeticion.text = "No se encontro ISBN"
+                }
+            } catch _ {
+                limpiar()
+                lblEstadoPeticion.text = "Ocurrió un error al obtener la información"
+            }
+        } else {
+            limpiar()
+            lblEstadoPeticion.text = "Error de conexión"
+        }
     }
     
 }
